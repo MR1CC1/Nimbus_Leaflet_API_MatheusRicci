e8b064e1920bf2bcd30ca36cb9128400a11134f3
MR1CC1 <matheusricci10@hotmail.com>
2023-11-09 17:44:21 -0300

    Adicionado Rota no App.js

--- nimbus-leaflet-api-matheus-ricci/src/App.js
+++ nimbus-leaflet-api-matheus-ricci/src/App.js
@@ -3,12 +3,19 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 import React from 'react'
 import Gerenciamento from './components/Pages/Gerenciamento/Gerenciamento';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Visualizacao from './components/Pages/Visualizacao/Visualizacao';
 
 function App() {
 
 
   return(
-    <Gerenciamento/>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Gerenciamento />} />
+      <Route path="/visualizacao" element={<Visualizacao />} />
+    </Routes>
+  </Router>
   )
  
 }
