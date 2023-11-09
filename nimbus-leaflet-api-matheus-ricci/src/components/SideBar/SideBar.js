e497da87c2184d6e8ee156bf17b35fe08ffcd6f2
MR1CC1 <matheusricci10@hotmail.com>
2023-11-09 17:44:53 -0300

    Recebendo as rotas no SideBar e deixando seu Style dinamico com a rota

--- nimbus-leaflet-api-matheus-ricci/src/components/SideBar/SideBar.js
+++ nimbus-leaflet-api-matheus-ricci/src/components/SideBar/SideBar.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../SideBar/SideBar.css';
 
 const SideBar = () => {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <div className='sidebar'>
-
+      <div id="cssmenu">
+        <ul>
+          <li style={isActive('/') ? { backgroundColor: '#F3F3F3', color: '#333333' } : {}}>
+            <Link to="/">Gerenciamento</Link>
+          </li>
+          <li style={isActive('/visualizacao') ? { backgroundColor: '#F3F3F3', color: '#333333' } : {}}>
+            <Link to="/visualizacao">Visualização</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
