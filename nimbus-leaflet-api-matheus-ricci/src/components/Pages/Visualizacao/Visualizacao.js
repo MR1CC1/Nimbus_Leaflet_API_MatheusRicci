cd19462925258c4d2fba9f02ddcc5ee415926ace
MR1CC1 <matheusricci10@hotmail.com>
2023-11-09 17:45:53 -0300

    Criado a pagina de Visualização de Pontos de Interesse

--- nimbus-leaflet-api-matheus-ricci/src/components/Pages/Visualizacao/Visualizacao.js
+++ nimbus-leaflet-api-matheus-ricci/src/components/Pages/Visualizacao/Visualizacao.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet';
+import SideBar from '../../SideBar/SideBar';
+import ToolBar from '../../ToolBar/ToolBar';
+import L from "leaflet";
 
 const Visualizacao = () => {
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+  });
+
   return (
-        <div>Visualizacao</div>
+    <div className='box'>
+      <SideBar />
+      <div className='header-map'>
+        <div className='header'>
+          <h1>Visualização dos pontos e áreas de interesse</h1>
+          <label>Label 1</label>
+          <input
+            type="number"
+            name="lat"
+          />
+          <label>Label 2</label>
+          <input
+            type="number"
+            name="lng"
+          />
+          <label>Label 3</label>
+          <input
+            type="number"
+            name="zoom"
+          />
+          <button >
+            Salvar
+          </button>
+        </div>
+        <MapContainer
+          center={{ lat: -22.914274, lng: -43.092566 }}
+          zoom={12}
+          scrollWheelZoom={true}
+        >
+          <ToolBar />
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </MapContainer>
+      </div>
+    </div>
   )
 }
 
