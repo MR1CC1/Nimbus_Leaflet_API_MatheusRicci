2f4918e7c13c93eef29835fc4daf1e58320e2005
Matheus Ricci <57235913+MR1CC1@users.noreply.github.com>
2023-11-12 19:39:35 -0300

    Enviando os Dados Para API (Pontos)

--- nimbus-leaflet-api-matheus-ricci/src/components/Headers/HeaderPoint.js
+++ nimbus-leaflet-api-matheus-ricci/src/components/Headers/HeaderPoint.js
@@ -3,11 +3,40 @@ import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import '../SideBar/SideBar.css';
 
 const HeaderPoint = () => {
+    // Estados para gerenciar descrição, latitude, longitude e o marcador no mapa
     const [descPoint, SetDescPoint] = useState('');
     const [latPoint, SetLatPoint] = useState(0);
     const [lngPoint, SetLngPoint] = useState(0);
     const [marker, setMarker] = useState(null);
 
+    // Função para salvar o ponto no servidor
+    const savePoint = async () => {
+        // Prepara os dados do ponto para envio
+        const pointData = {
+            desc: descPoint,
+            lat: latPoint,
+            lng: lngPoint
+        };
+
+        // Faz a requisição POST para salvar os dados
+        try {
+            const response = await fetch('http://localhost:3001/points', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(pointData),
+            });
+
+            if (response.ok) {
+                console.log('Ponto Salvo');
+            }
+        } catch (error) {
+            console.error('Erro ao Salvar o Ponto', error);
+        }
+    };
+
+    // Funções para manipular mudanças nos inputs de descrição, latitude e longitude
     const handleInputDesc = (e) => {
         SetDescPoint(e.target.value);
     };
@@ -15,6 +44,7 @@ const HeaderPoint = () => {
     const handleInputLat = (e) => {
         const newLat = parseFloat(e.target.value);
         SetLatPoint(newLat);
+        // Atualiza o marcador no mapa se as coordenadas são válidas
         if (!isNaN(newLat) && !isNaN(lngPoint)) {
             setMarker({ lat: newLat, lng: lngPoint });
         }
@@ -28,28 +58,32 @@ const HeaderPoint = () => {
         }
     };
 
+    // Efeito para atualizar o marcador no mapa quando as coordenadas mudam
     useEffect(() => {
         if (latPoint && lngPoint) {
             setMarker({ lat: latPoint, lng: lngPoint });
         }
     }, [latPoint, lngPoint]);
 
+    // Componente para lidar com eventos do mapa
     function MapClickHandler() {
         useMapEvents({
             click(e) {
+                // Define um novo marcador no local onde o mapa foi clicado
                 const newMarker = e.latlng;
                 setMarker(newMarker);
                 SetLatPoint(newMarker.lat);
                 SetLngPoint(newMarker.lng);
             },
         });
-        return null;
+        return null; // Este componente não renderiza nada visualmente
     }
 
     return (
         <div className='header-map'>
             <div className='header'>
                 <h1>Novo Ponto</h1>
+                {/* Campos de entrada para descrição, latitude e longitude */}
                 <label>Descrição</label>
                 <input
                     type="text"
@@ -68,10 +102,11 @@ const HeaderPoint = () => {
                     value={lngPoint || ''}
                     onChange={handleInputLng}
                 />
-                <button>
+                <button onClick={savePoint}>
                     Salvar
                 </button>
             </div>
+            {/* Container do mapa com uma camada de tile e um marcador representando o ponto */}
             <MapContainer
                 center={[10, 10]}
                 zoom={13}
